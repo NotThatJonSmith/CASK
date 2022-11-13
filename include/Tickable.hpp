a7f8e4dc6f86081e7b2d7bc380421f2680f7a9ca
Jonathan Smith <jondsmith22@gmail.com>
2022-11-13 05:21:24 -0600

    Tick now reports number of inner ticks

--- include/Tickable.hpp
+++ include/Tickable.hpp
@@ -6,7 +6,7 @@ class Tickable {
 
 public:
 
-    virtual inline void Tick() = 0;
+    virtual inline unsigned int Tick() = 0;
     virtual inline void BeforeFirstTick() { };
     virtual inline void Reset() { };
 
